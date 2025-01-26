7dc8f5eaee36cb4b221eaa6c0e11b4419a0d2595
gondiken <inarusjuan@gmail.com>
2025-01-26 23:12:33 +0100

    icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
+// src/pages/_app.tsx
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
