dace093c8257c9ea02db09cc9b7ebb7e825830bd
gondiken <inarusjuan@gmail.com>
2025-01-27 15:41:45 +0100

    fix vercel

---
+++ eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  settings: {
+    next: {
+      rootDir: ['./'],
+    },
+  },
+};
